adb9a3eb0e0e30336cca44907fb224f399ac6d79
pranaligole19-ux <pranaligole19@gmail.com>
2026-03-23 16:36:42 +0530

    Create Expt4b.py

---
+++ Expt4/Expt4b.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 16 04:49:45 2026
+
+@author: User
+"""
+
+# Take input from the user 
+string = input("Enter a string: ")
+# Reverse the string using slicing 
+reversed_string = string[::-1]
+# Print the result
+print("Reversed string:", reversed_string)
